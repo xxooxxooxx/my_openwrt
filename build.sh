df36ffa90eedacc5dbfa12d96dcb7fcc0d1b9373
xxooxxooxx <panchunmei30@gmail.com>
2021-02-02 15:53:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #
-echo test
+sudo apt-get update
+sudo apt-get install build-essential ccache ecj fastjar file g++ gawk \
+gettext git java-propose-classpath libelf-dev libncurses5-dev \
+libncursesw5-dev libssl-dev python python2.7-dev python3 unzip wget \
+python3-distutils python3-setuptools rsync subversion swig time \
+xsltproc zlib1g-dev -y
