d3b8e11e889c05a636b56b0ea47499497f9209cb
xxooxxooxx <panchunmei30@gmail.com>
2021-02-03 23:39:56 -0500

    update

--- build.sh
+++ build.sh
@@ -11,12 +11,25 @@ xsltproc zlib1g-dev -y
 wget https://downloads.openwrt.org/releases/$SDK_VERSION/targets/x86/64/openwrt-imagebuilder-$SDK_VERSION-x86-64.Linux-x86_64.tar.xz
 wget https://downloads.openwrt.org/releases/$SDK_VERSION/targets/x86/64/openwrt-sdk-$SDK_VERSION-x86-64_gcc-7.5.0_musl.Linux-x86_64.tar.xz
 tar xf openwrt-*.tar.xz
+H_PATH=$(pwd)
 cd openwrt-sdk-$SDK_VERSION-x86-64_gcc-7.5.0_musl.Linux-x86_64
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+#echo "src-link custom $H_PATH/package" >> $(pwd)/feeds.conf.default
+sed -i "\$a\src-link custom ${H_PATH}/package" $(pwd)/feeds.conf.default
+
+./scripts/feeds update custom
+./scripts/feeds install -a -p custom
+./scripts/feeds uninstall tinc
+./scripts/feeds install -p custom tinc
+
 make defconfig
-make package/tinc/{clean,compile} V=s
-cd -
+for i in ../package/* ; do
+  if [[ -d "$i" ]]; then
+    make package/${i##*/}{clean,compile} -j
+  fi
+done
+cd - &>/dev/null
 pwd>PATH.txt
 tree openwrt-sdk-$SDK_VERSION-x86-64_gcc-7.5.0_musl.Linux-x86_64/bin/packages/x86_64>LIST.txt
-cd -
