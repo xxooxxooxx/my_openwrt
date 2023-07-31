8d98389fea792cc3aae98ad47cf13dcde0d55788
xxooxxooxx <panchunmei30@gmail.com>
2023-07-31 17:54:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-SDK_VERSION=19.07.8
+SDK_VERSION=23.05.0-rc2
 
 sudo apt-get update
 sudo apt-get install build-essential ccache ecj fastjar file g++ gawk \
@@ -9,9 +9,9 @@ libncursesw5-dev libssl-dev python python2.7-dev python3 unzip wget \
 python3-distutils python3-setuptools rsync subversion swig time \
 xsltproc zlib1g-dev -y
 wget https://downloads.openwrt.org/releases/$SDK_VERSION/targets/x86/64/openwrt-imagebuilder-$SDK_VERSION-x86-64.Linux-x86_64.tar.xz
-wget https://downloads.openwrt.org/releases/$SDK_VERSION/targets/x86/64/openwrt-sdk-$SDK_VERSION-x86-64_gcc-7.5.0_musl.Linux-x86_64.tar.xz
+wget https://downloads.openwrt.org/releases/$SDK_VERSION/targets/x86/64/openwrt-sdk-$SDK_VERSION-x86-64_gcc-12.3.0_musl.Linux-x86_64.tar.xz
 tar xf openwrt-imagebuilder-$SDK_VERSION-x86-64.Linux-x86_64.tar.xz
-tar xf openwrt-sdk-$SDK_VERSION-x86-64_gcc-7.5.0_musl.Linux-x86_64.tar.xz
+tar xf openwrt-sdk-$SDK_VERSION-x86-64_gcc-12.3.0_musl.Linux-x86_64.tar.xz
 H_PATH=$(pwd)
 
 cd openwrt-sdk-$SDK_VERSION-x86-64_gcc-7.5.0_musl.Linux-x86_64
